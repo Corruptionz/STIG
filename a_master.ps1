e47368adea36752fe3c5df1833d7a0f9c57214f3
_corruptz <cmacleodd@gmail.com>
2020-12-31 15:34:25 -0800

    Updated reporting script

--- a_master.ps1
+++ a_master.ps1
@@ -1,7 +1,7 @@
 # ===========================================================================
 # Author: Corruptz
 # Creation Date : 11/8/2020
-# Last Updated  : 12/6/2020
+# Last Updated  : 1/5/2021
 # ===========================================================================
 # Description: 
 # The Windows 10 Security Technical Implementation Guide (STIG) 
@@ -90,34 +90,57 @@ foreach ($file in Get-ChildItem $severity_high_path -Filter *.ps1) {
 # Report
 Write-Host "[$(Get-TimeStamp)] Scan Complete."
 Write-Host "====================================================================================="
-Write-Host "There were [$high_severity_files_checked_count] CAT I (High Severity) STIGs scanned: "
+Write-Host ""
 
-foreach ($item in $high_severity_files_checked) {
-    Write-Host $item
+$results = @(
+    [PSCustomObject]@{
+        Severity = "CAT I (High)"
+        Total = 26
+        Checked = $high_severity_files_checked_count
+        Secure = $high_severity_findings_safe_count
+        Vulnerable = $high_severity_findings_misconfigured_count
+    },
+    [PSCustomObject]@{
+        Severity = "CAT II (Medium)"
+        Total = 246
+        Checked = 0
+        Secure = 0
+        Vulnerable = 0
+    },
+    [PSCustomObject]@{
+        Severity = "CAT III (Low)"
+        Total = 18
+        Checked = 0
+        Secure = 0
+        Vulnerable = 0
+    }
+)
+
+$results | Format-Table -AutoSize | Out-String | ForEach-Object {$_.Trim()}
+
+Write-Host ""
+Write-Host "There were [$high_severity_findings_safe_count] CAT I (High) STIGs found with secure configurations. "
+if ($high_severity_findings_safe_count -gt 0) {
+    foreach ($item in $high_severity_findings_safe) {
+        Write-Host $item
+    }
 }
-
-Write-Host "====================================================================================="
-Write-Host "There were [$high_severity_findings_safe_count] CAT I (High Severity) STIGs found not vulnerable."
-
-foreach ($item in $high_severity_findings_safe) {
-    Write-Host $item
+Write-Host ""
+Write-Host "There were [0] CAT II (Medium) STIGs found with secure configurations. "
+Write-Host ""
+Write-Host "There were [0] CAT III (Low) STIGs found with secure configurations. "
+Write-Host ""
+Write-Host "There were [$high_severity_findings_misconfigured_count] CAT I (High) STIGs found with vulnerable configurations."
+if ($high_severity_findings_misconfigured_count -gt 0) {
+    foreach ($item in $high_severity_findings_misconfigured) {
+        Write-Host $item
+    }
 }
-
+Write-Host ""
+Write-Host "There were [0] CAT II (Medium) STIGs found with vulnerable configurations."
+Write-Host ""
+Write-Host "There were [0] CAT III (Low) STIGs found with vulnerable configurations."
+Write-Host ""
 Write-Host "====================================================================================="
-Write-Host "There were [$high_severity_findings_misconfigured_count] CAT I (High Severity) STIGs found misconfigured / vulnerable."
-
-foreach ($item in $high_severity_findings_misconfigured) {
-    Write-Host $item
-}
-
-Write-Host "====================================================================================="
-Write-Host "There were [$high_severity_findings_missing_count] CAT I (High Severity) STIGs found missing / vulnerable. "
-
-foreach ($item in $high_severity_findings_missing) {
-    Write-Host $item
-}
-
-Write-Host "====================================================================================="
-Write-Host "[$(Get-TimeStamp)] Report complete!"
 
 # gpupdate /force
